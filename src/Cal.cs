8c614d95c82fbcef756e35ef5b8496345a9cc05e
Frank Erens <frank@synthi.net>
2013-09-14 16:02:58 +0200

    Initial commit

---
+++ src/Cal.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using DDay.iCal;
+using DDay.iCal.Serialization.iCalendar;
+
+class Cal {
+
+    const string kCalendarUriPrefix = "http://collegeroosters.uhasselt.be/";
+
+    const string kCalendarUriSuffix = ".ics";
+
+    static readonly String[] kCalendars = new String[] {
+        "677_2013_2014", // 3e bach informatica
+    };
+
+    static readonly String[] kCourseIds = new String[] {
+        "1537", // Natuurkunde en technologie
+        "1824", // Optica
+        "0173", // Mechanica
+        "1816", // Computernetwerken
+        "1588", // Wetenschapsfilosofie
+        "2941", // Kansrekening en statistiek
+        "2168", // Multimediatechnologie
+        "1602", // Logica en modeltheorie
+        "1303", // Software engineering
+        "2297", // Bacheloerproef
+    };
+
+    public static void Main() {
+        var calendars = kCalendars
+            .Select(x => new Uri(kCalendarUriPrefix + x + kCalendarUriSuffix))
+            .Select(x => iCalendar.LoadFromUri(x));
+
+        
+        var output = new iCalendar();
+        
+        foreach(var cc in calendars) {
+            foreach(var c in cc) {
+                foreach(var e in c.Events) {
+                    var match = Regex.Match(e.Description, @"\d\d\d\d");
+
+                    if(!match.Success ||
+                            kCourseIds.Any(x => x == match.Value)) {
+                        output.AddChild(e.Copy<Event>());
+                    }
+                }
+            }
+        }
+
+        var ser = new iCalendarSerializer();
+        Console.WriteLine(ser.SerializeToString(output));
+
+    }
+}
