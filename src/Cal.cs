545c3aa01a251da53ff0dcfff640f86287fed104
Frank Erens <frank@synthi.net>
2013-09-24 14:29:29 +0200

    Fixed a few warnings

--- src/Cal.cs
+++ src/Cal.cs
@@ -28,41 +28,6 @@ class Cal {
 
     const string kCalendarUriSuffix = ".ics";
 
-    static readonly String[] kCalendars = new String[] {
-        "677_2013_2014", // 3ba informatica (S) (1816, 1588, 2941, 2168, 1303, 2297)
-        "678_2013_2014", // 3ba wiskunde (S) (1602)
-        "628_2013_2014", // 1ba tew: hi (T) (1537)
-        "666_2013_2014", // 1ba wiskunde (T) (0173)
-        //"665_2013_2014", // 1ba fysica (T) (1824, 0173)
-    };
-
-    static readonly String[] kCourseIds = new String[] {
-        "1537", // Natuurkunde en technologie
-        "1824", // Optica
-        "0173", // Mechanica
-        "1816", // Computernetwerken
-        "1588", // Wetenschapsfilosofie
-        "2941", // Kansrekening en statistiek
-        "2168", // Multimediatechnologie
-        "1602", // Logica en modeltheorie
-        "1303", // Software engineering
-        "2297", // Bachelorproef
-    };
-
-    static readonly String[] kCourseShortHands = new String[] {
-        "NATEC", // Natuurkunde en technologie
-        "???",   // Optica
-        "MECH",  // Mechanica
-        "COMNE", // Computernetwerken
-        "MOCEB", // Wetenschapsfilosofie
-        "KSTAT", // Kansrekening en statistiek
-        "MMT",   // Multimediatechnologie
-        "LMT",   // Logica en modeltheorie
-        "???",   // Software engineering
-        "???",   // Bachelorproef
-    };
-
-    
     public static void Main(string[] args) {
         var serializer = new XmlSerializer(typeof(Schedule));
 
@@ -105,7 +70,7 @@ class Cal {
         }
 
         var ser = new iCalendarSerializer();
-        Console.WriteLine(ser.SerializeToString(output));
+        Console.Write(ser.SerializeToString(output));
 
     }
 }
